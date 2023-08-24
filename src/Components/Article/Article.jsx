728e82fec7a50b6564655097634f15c6d1586e1a
Artem Mandrin <a.v.mandrin@gmail.com>
2023-08-24 13:22:16 +0600

    Сделал вёрстку сртаницы с новостями
    Немного изменил структуру и написал стили

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -6,19 +6,19 @@ function Article(props) {
 
     return (
         <div className={Classes.Article}>
-            <div className={Classes.about}>
-                <p className={Classes.date}>{props.date}</p>
-                <p className={Classes.author}>{props.author}</p>
-            </div>
+            <div className={Classes.article_container}>
+                <div className={Classes.about}>
+                    <p className={Classes.date}>{props.date}</p>
+                    <p className={Classes.author}>{props.author}</p>
+                </div>
 
-            <div className={Classes.feed}>
-                <h3 className={Classes.title}>{props.title}</h3>
-                {/* разбить на абзацы входящую простыню */}
-                <div className={Classes.text}>{props.text}</div>
+                <div className={Classes.feed}>
+                    <h3 className={Classes.title}>{props.title}</h3>
+                    <div className={Classes.text}>{props.text}</div>
+                </div>
             </div>
-
             <div className={Classes.tags}>
-                {props.tags.map(tag => createElement('div', { className: Classes.tag, key:tag }, `#${tag}`))}
+                {props.tags.map(tag => createElement('div', { className: Classes.tag, key: tag }, `#${tag}`))}
             </div>
         </div>
     )
