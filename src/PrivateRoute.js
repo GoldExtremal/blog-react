f70ec2ede34667769480acaac9e4dbf4715094b4
Artem Mandrin <a.v.mandrin@gmail.com>
2023-09-06 14:49:48 +0600

    так не работает :)

---
+++ src/PrivateRoute.js
@@ -0,0 +1,10 @@
+import {Navigate, Outlet} from "react-router-dom";
+
+const isAuthorized = JSON.parse(localStorage.getItem('isAuthorized'))
+const PrivateRoute = () => {
+    return(
+        isAuthorized ? <Outlet/> : <Navigate to={"/login"}/>
+    );
+};
+
+export default PrivateRoute
