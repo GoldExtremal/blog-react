d4e70cf9546e368ae967e5c5d46fa4f4c608d8d9
Artem Mandrin <a.v.mandrin@gmail.com>
2023-09-12 01:15:36 +0600

    Renamed PrivateRoute component

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import {Navigate, Outlet} from "react-router-dom";
 const isAuthorized = JSON.parse(localStorage.getItem('isAuthorized'))
 const PrivateRoute = () => {
     return(
-        isAuthorized ? <Outlet/> : <Navigate to={"/login"}/>
+        isAuthorized ? <Outlet/> : <Navigate to={'/login'}/>
     );
 };
 
